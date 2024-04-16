c13a2d6a199a7e852c61f82b32cd8066d8e5581f
Paras <prsmahajan@gmail.com>
2024-04-16 16:21:03 +0530

    Added cors and some type safety

--- twitter-backend/src/app/index.ts
+++ twitter-backend/src/app/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { prismaClient } from "../clients/db";
@@ -9,6 +10,7 @@ export async function initServer() {
   const app = express();
 
   app.use(bodyParser.json());
+  app.use(cors());
 
   const graphqlServer = new ApolloServer({
     typeDefs: `
